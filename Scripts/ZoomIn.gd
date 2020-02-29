738bbb0d90dc746e913745dd11ebf86f1e78cc4c
daveymclain <braveheart_52@hotmail.com>
2020-02-29 11:59:28 +0000

    Bug fix: Reset zoom when changing floor

--- Scripts/ZoomIn.gd
+++ Scripts/ZoomIn.gd
@@ -1,7 +1,21 @@
 extends Button
 
 
+var ground_floor = true
+onready var top_floor_node = get_node("/root/App/TopFloor")
+onready var ground_floor_node = get_node("/root/App/GroundFloor")
 
-func _on_ZoomIn_pressed():
-	get_node("/root/App/GroundFloor").scale = Vector2(1, 1)
-	get_node("/root/App/GroundFloor").position = Vector2(0, 0)
+
+func _on_Top_Floor_pressed():
+	if ground_floor:
+		top_floor_node.scale = Vector2(1,1)
+		text = "Top Floor"
+		get_node("/root/App/GroundFloor").position = Vector2(1000, 0)
+		get_node("/root/App/TopFloor").position = Vector2(0, 0)
+		ground_floor = false
+	else:
+		ground_floor_node.scale = Vector2(1,1)
+		text = " Ground Floor "
+		get_node("/root/App/GroundFloor").position = Vector2(0, 0)
+		get_node("/root/App/TopFloor").position = Vector2(1000, 0)
+		ground_floor = true
